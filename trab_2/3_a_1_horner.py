9019c253b046c42a29731d1243d407f8dbf5540f
mari-carvalho <03950025081@edu.udesc.br>
2024-11-11 19:27:58 -0300

    iniciando trab2

---
+++ trab_2/3_a_1_horner.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math as mt
+
+
+# informações da formação:
+
+q = 185 # BOPD
+h = 114 #ft
+phi = 0.28
+ct = 4.1*10^-6 # psi^-1
+mi_o = 2.2 #cp
+Bo = 1.1 # RB/STB
+rw = 0.50 # ft
+pwf = 2820 # psia
+tp = 540 #hrs
